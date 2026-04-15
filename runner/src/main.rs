3b9828d61e131a739ced3a09c7c4e169ec42a24c
WillTheBigCodeMan <will.lucas.evans@outlook.com>
2026-04-15 15:51:16 +0100

    good progress

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,18 +1,23 @@
+use chrono::DateTime;
 use core::time;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::Error;
 use std::cmp::max;
+use std::cmp::min;
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::ops::Sub;
 use std::path::Path;
 use std::process::Command;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::mpsc;
 use std::thread;
-use std::thread::JoinHandle;
+use std::time::Duration;
+use std::time::Instant;
 use std::time::SystemTime;
 use threadpool::ThreadPool;
 
@@ -22,6 +27,7 @@ struct Config {
     args: Vec<Vec<String>>,
     problem_names: Vec<String>,
     negate: bool,
+    oxide: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -32,6 +38,7 @@ struct Configs {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Meta {
     name: String,
+    params: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -51,12 +58,62 @@ struct BenchmarkResult {
     problem: Problem,
     total_time: f64,
     times: Vec<Section>,
+    param_runs: Vec<ParamRun>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ParamRun {
+    name: String,
+    total_time: f64,
+    times: Vec<Section>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Section {
     name: String,
     time: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ConjureStats {
+    computer: String,
+    conjureVersion: String,
+    essence: String,
+    essenceParams: Vec<String>,
+    runsolverInfo: RunsolverInfo,
+    savilerowInfo: SavilerowInfo,
+    savilerowLogs: SavilerowLogs,
+    savilerowOptions: Vec<String>,
+    savilerowVersion: String,
+    solver: String,
+    solverOptions: Vec<String>,
+    status: String,
+    timestamp: String,
+    totalTime: f64,
+    useExistingModels: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct RunsolverInfo {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct SavilerowInfo {
+    SavileRowClauseOut: String,
+    SavileRowTimeOut: String,
+    SavileRowTotalTime: String,
+    SolverNodes: String,
+    SolverSatisfiable: String,
+    SolverSetupTime: String,
+    SolverSolutionsFound: String,
+    SolverSolveTime: String,
+    SolverTimeOut: String,
+    SolverTotalTime: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct SavilerowLogs {
+    exitCode: i64,
+    stdout: Vec<String>,
 }
 
 fn main() {
@@ -85,7 +142,7 @@ fn main() {
         }
     };
 
-    let pool = ThreadPool::new(max(1, num_cpus::get() - 1));
+    let pool = ThreadPool::new(min(2, num_cpus::get() - 8));
 
     let problems = find_problems(problems);
     run_benchmarks(problems, configs, output, pool);
@@ -197,43 +254,191 @@ fn run_all(
         let command = format!("{} {}", config.conjure_path, args.join(" "));
 
         for p in problems {
+            if (!config.negate && !config.problem_names.contains(&p.meta.name))
+                || (config.negate && config.problem_names.contains(&p.meta.name))
+            {
+                continue;
+            }
+
             let p = p.clone();
             let command = command.clone();
             let out = Arc::clone(&out);
-            pool.execute(move || {
-                if let Some(res) = run_one_problem(p, command) {
+
+            let re = Regex::new("[^/]*$").unwrap();
+            if p.meta.params != "" {
+                let param_path = re.replace(&p.path, p.meta.params.clone());
+                let p_idx;
+                {
                     let mut out = out.lock().unwrap();
                     let r: &mut Results = out.get_mut(idx).unwrap();
-                    r.results.push(res);
+                    r.results.push(BenchmarkResult {
+                        problem: p.clone(),
+                        total_time: -1.0,
+                        times: Vec::new(),
+                        param_runs: Vec::new(),
+                    });
+                    p_idx = r.results.len() - 1;
                 }
-            });
+                for entry in fs::read_dir(param_path.as_ref()).unwrap() {
+                    let out = Arc::clone(&out);
+                    if let Ok(e) = entry {
+                        let path = e.path();
+                        let path = path.to_str().unwrap().to_string();
+                        let com = command.clone();
+                        let prob = p.clone();
+                        pool.execute(move || {
+                            if let Some(res) = run_one_problem(&prob, com, config.oxide, path) {
+                                let mut out = out.lock().unwrap();
+                                let r: &mut Results = out.get_mut(idx).unwrap();
+                                let b: &mut BenchmarkResult = r.results.get_mut(p_idx).unwrap();
+                                b.param_runs.push(ParamRun {
+                                    name: e.file_name().to_str().unwrap().to_string(),
+                                    total_time: res.total_time,
+                                    times: res.times,
+                                });
+                            }
+                        });
+                    }
+                }
+
+                // Squash into one result with all the different params
+            } else {
+                pool.execute(move || {
+                    if let Some(res) = run_one_problem(&p, command, config.oxide, "".to_string()) {
+                        let mut out = out.lock().unwrap();
+                        let r: &mut Results = out.get_mut(idx).unwrap();
+                        r.results.push(res);
+                    }
+                });
+            }
         }
     }
 }
 
-fn run_one_problem(problem: Problem, command: String) -> Option<BenchmarkResult> {
+// Change this to have param file
+fn run_one_problem(
+    problem: &Problem,
+    command: String,
+    oxide: bool,
+    param: String,
+) -> Option<BenchmarkResult> {
     let mut cmd = Command::new("bash");
     let start = SystemTime::now();
+    let temp = format!(
+        "{}-{}",
+        problem.meta.name,
+        start
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap_or(Duration::new(0, 0))
+            .as_secs_f64()
+    );
+    if let Err(_) = fs::create_dir(&temp) {
+        return None;
+    }
+
+    let mut extra_opts = "";
+    if oxide {
+        extra_opts = "--log --logfile conjure-oxide.log"
+    }
+
+    let start = Instant::now();
+
+    let mut param_path = String::new();
+    if param != "" {
+        param_path = "../".to_string() + &param;
+    }
+
     let output = cmd
         .arg("-c")
-        .arg(format!("{} {}", command, problem.path))
+        .arg(format!(
+            "cd {}; {} {} ../{} {}",
+            temp, command, extra_opts, problem.path, param_path
+        ))
         .output();
-    return match output {
-        Ok(o) => {
-            if o.stderr.len() != 0 {
-                None
-            } else {
-                if let Ok(dur) = start.elapsed() {
-                    Some(BenchmarkResult {
-                        problem: problem,
-                        times: vec![],
-                        total_time: dur.as_secs_f64(),
-                    })
-                } else {
-                    None
+    let elapsed = start.elapsed();
+    let mut times: Vec<Section> = Vec::new();
+
+    if oxide {
+        let log_file =
+            fs::read_to_string(format!("{}/conjure-oxide.log", temp)).unwrap_or(String::new());
+
+        let mut last_stamp = Ok(DateTime::from_timestamp_nanos(0).fixed_offset());
+        for line in log_file.split("\n") {
+            if line.contains("INFO") {
+                let segs = line.split(" ").collect::<Vec<&str>>();
+                let time_stamp = segs.get(0).unwrap();
+                let time_stamp = DateTime::parse_from_rfc3339(time_stamp);
+                if line.contains("Rewriting") {
+                    last_stamp = time_stamp.clone();
+                }
+
+                if line.contains("Rewritten") {
+                    times.push(Section {
+                        name: "Rewriting".to_string(),
+                        time: time_stamp
+                            .unwrap()
+                            .sub(last_stamp.unwrap())
+                            .as_seconds_f64(),
+                    });
+                    last_stamp = time_stamp.clone();
+                }
+
+                if line.contains("Solutions") {
+                    times.push(Section {
+                        name: "Solver".to_string(),
+                        time: time_stamp
+                            .unwrap()
+                            .sub(last_stamp.unwrap())
+                            .as_seconds_f64(),
+                    });
                 }
             }
         }
+    } else {
+        for entry in fs::read_dir(format!("{}/conjure-output", temp)).unwrap() {
+            if entry
+                .as_ref()
+                .unwrap()
+                .file_name()
+                .to_str()
+                .unwrap()
+                .contains(".json")
+            {
+                let stats = fs::read_to_string(entry.unwrap().path()).unwrap();
+                let stats: Result<ConjureStats, Error> = serde_json::from_str(&stats);
+                times.push(Section {
+                    name: "Savile Row".to_string(),
+                    time: stats
+                        .as_ref()
+                        .unwrap()
+                        .savilerowInfo
+                        .SavileRowTotalTime
+                        .parse()
+                        .unwrap(),
+                });
+                times.push(Section {
+                    name: "Solver".to_string(),
+                    time: stats
+                        .unwrap()
+                        .savilerowInfo
+                        .SolverTotalTime
+                        .parse()
+                        .unwrap(),
+                });
+                break;
+            }
+        }
+    }
+
+    let _ = fs::remove_dir_all(temp);
+
+    return match output {
+        Ok(_) => Some(BenchmarkResult {
+            problem: problem.clone(),
+            times: times,
+            total_time: elapsed.as_secs_f64(),
+            param_runs: Vec::new(),
+        }),
         Err(_) => None,
     };
 }
